075f56bf760445800d3e6b5c491ccb66d5ad7e9d
zrliang <32066396+zrliang@users.noreply.github.com>
2020-07-20 19:19:38 +0800

    add annotation

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -15,12 +15,13 @@ MachineLimit=[[1,0,1],[1,1,1],[1,0,1],[1,1,1],[1,1,0],
 Processtime=[[80,0,60],[75,86,94],[25,0,96],[78,95,89],[45,78,0],
               [12,0,65],[55,99,87],[11,0,16],[0,16,45],[43,56,21]]
 
+
 #指派機器Function
 def GetMachineNumber(MachineLimit):
     
-    import random
     #選機隨機碼
-    SelectJobCode=random.random()
+    import random
+    SelectJobCode=random.random() #0~1亂數
     #單份機率
     sum = 0
     for i in MachineLimit:
@@ -37,19 +38,19 @@ def GetMachineNumber(MachineLimit):
     
     #工作派給機器(j)
     FinalMachineJ1=0
-    sequence=[0,1,2]
+    sequence=[0,1,2] #機1.2.3
     for j in sequence:
         if(MachineLimit[j]==1):
             k-=1
         if(k==0):
-            FinalMachineJ1=j+1
+            FinalMachineJ1=j+1 #加回來
             break             
     return FinalMachineJ1
 
-#指派10工件
+##(呼叫Function)指派10工件給3機
 JobResult=[]
 for s in MachineLimit:
-    JobResult.append(GetMachineNumber(s))
+    JobResult.append(GetMachineNumber(s)) #參數 工件一:[1,0,1]...
 
 #分類依機台
 MachineNumber=1
@@ -64,12 +65,14 @@ for w in JobResult:
     else:
         M3.append(MachineNumber)
     MachineNumber+=1
+print("步驟一")   
 print(M1)
 print(M2)
 print(M3)
+
 #步驟二
 #決定每個Job在三個機台中的先後順序
-
+ 
 #產生順序隨機碼，進行排序
 import random
 OrderJobM1=[]
@@ -78,7 +81,7 @@ OrderJobM3=[]
 for x in range(len(M1)):
     OrderJobM1.append(random.random())
 cM1 = list(zip(M1,OrderJobM1))  #兩個一維轉成一個二維
-nM1=sorted(cM1,key=(lambda x:x[1]),reverse=True) #二維排序
+nM1=sorted(cM1,key=(lambda x:x[1]),reverse=True) #二維排序(x[1]針對欄位二)
 #print(nM1)  #排序結果
 for x in range(len(M2)):
     OrderJobM2.append(random.random())
@@ -92,31 +95,32 @@ cM3 = list(zip(M3,OrderJobM3))  #兩個一維轉成一個二維
 nM3=sorted(cM3,key=(lambda x:x[1]),reverse=True) #二維排序
 #print(nM3)  #排序結果
 
-# 工作順序
+print("步驟二")  
+# 只取出排序後的工件欄位(nnM?)
 nnM1=[]
 for a in range(len(nM1)):
    # print(nM1[a][0]) >> Job幾 
     nnM1.append(nM1[a][0])
-
+print(nnM1)
 nnM2=[]
  # 工作順序
 for a in range(len(nM2)):
    # print(nM1[a][0]) >> Job幾 
     nnM2.append(nM2[a][0])
-#print(nnM1)
+print(nnM2)
     
 nnM3=[]
  # 工作順序
 for a in range(len(nM3)):
    # print(nM1[a][0]) >> Job幾 
     nnM3.append(nM3[a][0])
-#print(nnM1)   
+print(nnM3)   
     
     
     
 #步驟三
 #計算每個Job的開始與結束時間
-
+print("步驟三")  
 ###---------M1-------
 
 # 開始時間
@@ -134,7 +138,7 @@ M1EndAnswer=M1t
 M1EndT=[]
 for b in range(len(nnM1)):
    # M1StartT.append(Processtime[nnM1[b]-1][0])
-    M1EndAnswer+=Processtime[nnM1[b]-1][0]
+    M1EndAnswer+=Processtime[nnM1[b]-1][0]   #0 [80,0,60] 取第一個欄位
     M1EndT.append(M1EndAnswer)
 # 合併
 M1Answer = list(zip(nnM1,M1StartT,M1EndT))
@@ -151,7 +155,7 @@ M2StartT=[]
 M2StartT.append(M2t)
 for b in range(len(nnM2)-1):
    # M1StartT.append(Processtime[nnM1[b]-1][0])
-    M2StartAnswer+=Processtime[nnM2[b]-1][1]
+    M2StartAnswer+=Processtime[nnM2[b]-1][1]    #1 [80,0,60] 取第二個欄位
     M2StartT.append(M2StartAnswer)
     
 # 結束時間
@@ -175,7 +179,7 @@ M3StartT=[]
 M3StartT.append(M3t)
 for b in range(len(nnM3)-1):
    # M1StartT.append(Processtime[nnM1[b]-1][0])
-    M3StartAnswer+=Processtime[nnM3[b]-1][2]
+    M3StartAnswer+=Processtime[nnM3[b]-1][2]    #2 [80,0,60] 取第三個欄位         
     M3StartT.append(M3StartAnswer)
     
 # 結束時間
@@ -189,8 +193,15 @@ for b in range(len(nnM3)):
 M3Answer = list(zip(nnM3,M3StartT,M3EndT))
 print(M3Answer)   
 
-
-
+#-------Makespan----------
+'''
+if(M1Answer>M2Answer and M1Answer>M3Answer):
+    print("M1"+" "+ str(M1Answer))
+elif(M2Answer>M1Answer and M2Answer>M3Answer):
+    print("M2"+" "+ str(M2Answer))
+else:
+    print("M3"+" "+ str(M3Answer))
+''' 
 
 
 
--- test.py
+++ test.py
@@ -4,19 +4,112 @@ Spyder Editor
 
 This is a temporary script file.
 """
+#步驟一
+#將每個Job 亂數指派到Machine 1~3之一
+
+#工件機器限制 K
+MachineLimit=[[1,0,1],[1,1,1],[1,0,1],[1,1,1],[1,1,0],
+              [1,0,1],[1,1,1],[1,0,1],[0,1,1],[1,1,1]]
+
+#工作時間
 Processtime=[[80,0,60],[75,86,94],[25,0,96],[78,95,89],[45,78,0],
               [12,0,65],[55,99,87],[11,0,16],[0,16,45],[43,56,21]]
 
-cM1=[]
-M1=[5,6]
+
+#指派機器Function
+def GetMachineNumber(MachineLimit):
+    
+    #選機隨機碼
+    import random
+    SelectJobCode=random.random() #0~1亂數
+    #單份機率
+    sum = 0
+    for i in MachineLimit:
+        sum += i
+    PJ1 =1/sum
+    #決定是第幾份(k)
+    k=1 #<預設為第一份>
+    TotalPJ1=PJ1
+    while(SelectJobCode>TotalPJ1):
+        k+=1
+        TotalPJ1=PJ1*k
+    
+    #print(k)
+    
+    #工作派給機器(j)
+    FinalMachineJ1=0
+    sequence=[0,1,2] #機1.2.3
+    for j in sequence:
+        if(MachineLimit[j]==1):
+            k-=1
+        if(k==0):
+            FinalMachineJ1=j+1 #加回來
+            break             
+    return FinalMachineJ1
+
+##(呼叫Function)指派10工件給3機
+JobResult=[]
+for s in MachineLimit:
+    JobResult.append(GetMachineNumber(s)) #參數 工件一:[1,0,1]...
+
+#分類依機台
+MachineNumber=1
+M1=[]
+M2=[]
+M3=[]
+for w in JobResult:
+    if(w==1):
+        M1.append(MachineNumber)
+    elif(w==2):
+        M2.append(MachineNumber)
+    else:
+        M3.append(MachineNumber)
+    MachineNumber+=1
+print(M1)
+print(M2)
+print(M3)
+
+#步驟二
+#決定每個Job在三個機台中的先後順序
+ 
+#產生順序隨機碼，進行排序
 import random
 OrderJobM1=[]
-for x in range(len(M1)):
-    OrderJobM1.append(random.random())
-cM1 = list(zip(M1,OrderJobM1))  #兩個一維轉成一個二維
-nM1=sorted(cM1,key=(lambda x:x[1]),reverse=True) #二維排序
+OrderJobM2=[]
+OrderJobM3=[]
 
+Machine=[]
+Machine.append(M1)
+Machine.append(M2)
+Machine.append(M3)
+def GetMachineJobOrder(M1):
+    for x in range(len(M1)):
+        OrderJobM1.append(random.random())
+    cM1 = list(zip(M1,OrderJobM1))  #兩個一維轉成一個二維
+    nM1=sorted(cM1,key=(lambda x:x[1]),reverse=True) #二維排序(x[1]針對欄位二)
+    #print(nM1)  #排序結果
+    return nM1
 
-print(Processtime[:][0])
+OrderResult=[]
+for q in Machine:
+    OrderResult.append(GetMachineJobOrder(q))
+print(OrderResult)
+# 只取出排序後的工件欄位(nnM?)
+nnM1=[]
+for a in range(len(nM1)):
+   # print(nM1[a][0]) >> Job幾 
+    nnM1.append(nM1[a][0])
 
-
+nnM2=[]
+ # 工作順序
+for a in range(len(nM2)):
+   # print(nM1[a][0]) >> Job幾 
+    nnM2.append(nM2[a][0])
+#print(nnM1)
+    
+nnM3=[]
+ # 工作順序
+for a in range(len(nM3)):
+   # print(nM1[a][0]) >> Job幾 
+    nnM3.append(nM3[a][0])
+#print(nnM1)   
